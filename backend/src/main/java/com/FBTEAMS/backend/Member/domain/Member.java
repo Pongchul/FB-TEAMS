398faa6bc085d7fd172c472b3de6c5be7c435538
inchul7511@gmail.com <inchul7511@gmail.com>
2024-03-27 20:05:10 +0900

    Featuring : Added domain member logic.

--- backend/src/main/java/com/FBTEAMS/backend/Member/domain/Member.java
+++ backend/src/main/java/com/FBTEAMS/backend/Member/domain/Member.java
@@ -1,13 +1,12 @@
 package com.FBTEAMS.backend.Member.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.*;
 
 @Entity
@@ -18,5 +17,14 @@ public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String socialLoginId;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String nickname;
+
+    @Enumerated(value = STRING)
+    private MemberState status;
 
 }
